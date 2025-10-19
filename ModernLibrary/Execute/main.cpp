3808039cb396383b3275626e2ecfd84fc661cf73
RockingHeart <rockingchords@gmail.com>
2025-10-19 16:26:04 +0800

    up

--- ModernLibrary/Execute/main.cpp
+++ ModernLibrary/Execute/main.cpp
@@ -6,95 +6,9 @@ char arr1[32]{};
 wchar_t arr2[5]{};
 import <windows.h>;
 
-template <class Ty>
-struct ailor {
-	using value_type = Ty;
-
-	constexpr Ty* allocate(size_t n) {
-		return static_cast<Ty*>(::operator new[](n * sizeof(Ty),
-			std::align_val_t(alignof(Ty))));
-	}
-
-	constexpr void deallocate(void* ptr, size_t) noexcept {
-		::operator delete[](ptr, std::align_val_t(alignof(Ty)));
-	}
-
-	// For allocator completeness
-	using propagate_on_container_move_assignment = std::true_type;
-	using is_always_equal = std::true_type;
-};
-
-static void test_std() {
-	static std::basic_string<wchar_t, std::char_traits<wchar_t>, ailor<wchar_t>> not_optimize;
-	auto stime = GetTickCount64();
-	for (size_t i = 0; i < 50000000; i++) {
-		std::basic_string<wchar_t, std::char_traits<wchar_t>, ailor<wchar_t>> str1 = L"Hell";
-		not_optimize = str1;
-	}
-	auto etime = GetTickCount64();
-	std::cout << "Std String: " << (etime - stime) << '\n';
-}
-
-static void test_my() {
-	static dast::wstring<ailor<wchar_t>> not_optimize;
-	auto stime = GetTickCount64();
-	for (size_t i = 0; i < 50000000; i++) {
-		dast::wstring<ailor<wchar_t>> str1 = L"Hell";
-		not_optimize = str1;
-	}
-	auto etime = GetTickCount64();
-	std::cout << "My String: " << (etime - stime) << '\n';
-}
-
 int main() {
-	test_std();
-	test_my();
-	/*string strTs = L"Hello";
-	std::cout << sizeof(string<char>) << '\n';
-	string<char, value_traits::no_residue> str = "A";
-	std::cout << str.max_size() << '\n';
-	str.resize(50, 'a');
-	str.resize(100, 'a');
-	std::cout << str.const_string() << '\n';
-	for (size_t i = 0; i <= 50; i++) {
-		str += 'B';
-	}
-	for (auto& c : str) {
-		std::cout << c << ' ';
-	} std::cout << '\n';
-	std::cout << str.size() << '\n';
-	std::cout << str.tick('B', 0, str.size()) << '\n';
-	str[4] = 'T';
-	std::cout << str.index('T', 0, str.size()).result << '\n';
-	str[38] = 'D';
-	std::cout << str.index('D', str.size(), 0).result << '\n';
-	std::cout << str.const_string() << '\n';
-	std::string std_str1 = str.to<std::string>();
-	std::cout << std_str1 << '\n';
-	std::string std_str2 = str.to<std::string>(4);
-	std::cout << std_str2 << '\n';
-	std::cout << str.index(40) << '\n';
-	str.resize(30, 'a');
-	str.restore_cache_mode();
-	std::cout << str.const_string() << '\n';
-	string<char, value_traits::remain> str1 = "Hello!!! My World, I like u, Do you like me.....?";
-	str1.resize(100, 'a');
-	string<char, value_traits::remain> str2 = str1;
-	string<char, value_traits::remain> str3 = std::move(str1);
-	std::cout << str1.const_string() << '\n';
-	std::cout << str2.const_string() << '\n';
-	std::cout << str3.const_string() << '\n';
-	string<char, value_traits::remain> str4 = 'A';
-	std::cout << (str4 == 'A') << '\n';
-	constexpr size_t resu = __builtin_strlen("HG");
-	string<char, value_traits::remain> str5 = "Hello!!!";
-	str5.swap(str3);
-	std::cout << str3.const_string() << '\n';
-	std::cout << str5.const_string() << '\n';
-	string<char, value_traits::enhance> str6 = "Hello!!!A";
-	string<char, value_traits::enhance> str7 = "Hello!!";
-	str6 = std::move(str7);
-	std::cout << str6.const_string() << '\n';
-	std::cout << (str7 == "Hello!!!A") << '\n';*/
+	dast::cstring str = "Hello";
+	str.resize(100, '.');
+	std::cout << str.const_string();
 	return 0;
 }
